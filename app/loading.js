f8aa83384afe91dc64d7b8463ee94de5eba7f4b0
emad-desoky <deskoemad@gmail.com>
2024-11-13 19:24:32 +0200

    Create loading.js

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <h1 className="text-8xl flex items-center justify-center mt-64 text-red-800">
+      Logging in........
+    </h1>
+  );
+};
+
+export default loading;
